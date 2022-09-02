0b455437eb1d85e31b80e8ba7f7b034c1bcb491d
robertovidal <66501561+robertovidal@users.noreply.github.com>
2022-09-01 18:30:47 -0600

    Cambio en el api

--- controlador/api.js
+++ controlador/api.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const ActividadRouter = require('./ActividadControlador');
 const ActivoRouter = require('./ActivoControlador');
 const JuntaDirectivaRouter = require('./JuntaDirectivaControlador');
-const OrganizacionRouter = require('./OrganizacionRutas');
-const PaginaRouter = require('./PaginaRutas');
-const PersonaRouter = require('./PersonaRutas');
-const ProyectoRouter = require('./ProyectoRutas');
-const UsuarioRouter = require('./UsuarioRutas');
+const OrganizacionRouter = require('./OrganizacionControlador');
+const PaginaRouter = require('./PaginaControlador');
+const PersonaRouter = require('./PersonaControlador');
+const ProyectoRouter = require('./ProyectoControlador');
+const UsuarioRouter = require('./UsuarioControlador');
 
 router.use('/actividad', ActividadRouter);
 router.use('/activo', ActivoRouter);
