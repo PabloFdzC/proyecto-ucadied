36e13484794259f9418ae6015511b094e6fd11d1
PabloFdzC <pablof5181@gmail.com>
2022-09-12 15:24:56 -0600

    Arreglo despliegue intento 11

--- conexion_base.js
+++ conexion_base.js
@@ -1,8 +1,19 @@
+const app = express();
 const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('proyectoucadied', 'root', '1234', {
+var nombreBase = 'proyectoucadied';
+var usuario = 'root';
+var clave = '1234';
+var info = {
     host: 'localhost',
     dialect: 'mysql'
-});
+};
+
+if (app.get('env') === 'production') {
+    usuario = 'admin';
+    clave = 'Pr0y3ct0Ucadied!';
+    info.host = '10.0.1.236';
+}
+
+const sequelize = new Sequelize(nombreBase, usuario, clave, info);
 
 module.exports = sequelize;
