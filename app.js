c2a2d502465e1d3f76af45fd8f7364f40dddb738
PabloFdzC <pablof5181@gmail.com>
2022-09-21 11:00:45 -0600

    Arreglo en sesiones

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const ApiRouter = require('./controlador/api');
 const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 
 const app = express();
 app.use(cors({
@@ -18,22 +19,29 @@ var sess = {
     secret: 'm!S3ssi0nn0de',
     resave: false,
     saveUninitialized: true,
-    cookie: {  }
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000
+    }),
   }
+
 
 if (app.get('env') === 'production') {
     app.set('trust proxy', 1);
     sess.cookie.secure = true;
-     app.use(express.static(path.join(__dirname, 'vista/build')));
+    app.use(express.static(path.join(__dirname, 'vista/build')));
+    app.use(session(sess));
+    app.use('/', ApiRouter);
 
     app.get('/*', function (req, res) {
         res.sendFile(path.join(__dirname, 'vista/build', 'index.html'));
     });
+} else {
+    app.use(session(sess));
+    app.use('/', ApiRouter);
 }
 
-app.use(session(sess));
 
-app.use('/', ApiRouter);
 
 
 
